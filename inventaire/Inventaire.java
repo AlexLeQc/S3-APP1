be6e3a1a30a1078b3c1316bc696d98602c14d290
raphbouc <bour0703@usherbrooke.ca>
2023-05-03 23:33:50 -0400

    Inventaire 2/2

--- inventaire/Inventaire.java
+++ inventaire/Inventaire.java
@@ -43,4 +43,45 @@ public class Inventaire {
     public int getQuantiteIngredient(){
         return entrepot.size();
     }
+    public void consommerRecette(IngredientPlat recette, int quantitePlat, double proportion) throws IngredientException {
+        if (recette == null) {
+            throw new IngredientException("Recette ne peut pas être null");
+        }
+        if (quantitePlat < 0) {
+            throw new IngredientException("Impossible de consommer une quantité négative de plats");
+        }
+        if (proportion < 0) {
+            throw new IngredientException("Impossible de consommer des plats à proportion négative");
+        }
+        if (proportion > 1) {
+            throw new IngredientException("Proportion de la recette ne peut pas être > 1");
+        }
+
+        for (Ingredient ingredientRecette : recette.getIngredients()) {
+            Ingredient ingredientCongelateur = entrepot.get(ingredientRecette.getNom());
+
+            if (ingredientCongelateur == null) {
+                throw new IngredientException("Ingrédient n'existe pas dans l'inventaire");
+            }
+            double qtyRecette = ingredientRecette.getQuantite() * quantitePlat * proportion;
+            double qtyInventaire = ingredientCongelateur.getQuantite();
+
+            if (qtyInventaire < qtyRecette) {
+                throw new IngredientException("Ingrédients manquants dans l'inventaire");
+            }
+            ingredientCongelateur.setQuantite(qtyInventaire - qtyRecette);
+        }
+    }
+    public static void vider() {
+        if (instance != null) {
+            instance.entrepot.clear();
+            instance = null;
+        }
+    }
+
+
+    public String toString() {
+        return "Inventaire: " + entrepot;
+    }
 }
+
