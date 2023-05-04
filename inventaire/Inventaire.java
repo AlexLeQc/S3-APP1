23bdb5ce9780ce61d60ec12166dd9b9d8f6eec46
raphbouc <bour0703@usherbrooke.ca>
2023-05-03 20:41:30 -0400

    Inventaire 1/2

--- inventaire/Inventaire.java
+++ inventaire/Inventaire.java
@@ -1,15 +1,46 @@
 package inventaire;
 
 import ingredients.Ingredient;
+import ingredients.exceptions.IngredientException;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Inventaire {
-    private ArrayList<Ingredient> lesIngredients = new ArrayList<Ingredient>();
-
-    public void ajouter (Ingredient ingredient)
-    {
-        lesIngredients.add(ingredient);
+    private static Inventaire instance;
+    private HashMap<String, Ingredient> entrepot;
+    public static synchronized Inventaire getInstance(){
+        if (instance == null){
+            instance = new Inventaire();
+        }
+        return instance;
     }
-
+    private Inventaire(){
+        entrepot = new HashMap<>();
+    }
+    public void ajoutIngredient(Ingredient[] ingredients) throws IngredientException {
+        if(ingredients == null){
+            throw new IngredientException("Impossible d'ajouter une liste null à l'inventaire");
+        }
+        for (Ingredient ingredient : ingredients) {
+            ajoutIngredient(ingredients);
+        }
+    }
+    public void ajoutIngredient(Ingredient ingredient) throws IngredientException{
+        if (ingredient == null) {
+            throw new IngredientException("Impossible d'ajouter un ingredient à l'inventaire");
+        } else {
+            if (entrepot.containsKey(ingredient.getNom())){
+                Ingredient ingredient2 = entrepot.get(ingredient.getNom());
+                ingredient2.setQuantite(ingredient2.getQuantite() + ingredient.getQuantite()) ;
+            } else {
+                entrepot.put(ingredient.getNom(), ingredient);
+            }
+        }
+    }
+    public Ingredient getIngredient(String ingredientNom){
+        return entrepot.get(ingredientNom);
+    }
+    public int getQuantiteIngredient(){
+        return entrepot.size();
+    }
 }
